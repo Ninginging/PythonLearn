6cb19927d91bd6a78b3368ae59f36f96e4031d48
Ninginging <86232694+Ninginging@users.noreply.github.com>
2021-07-01 20:26:52 +0800

    homework06完成

--- work06.py
+++ work06.py
@@ -13,7 +13,7 @@ def statistical_words(__dir, files_list):
         word_list = work04.get_words_list(temp_string)
         words_count_dict = Counter(word_list)
         print('%s当中出现次数前三多的单词和出现数量是' % Diary_Files_List[i], words_count_dict.most_common(3))
-        temp_file.close
+        temp_file.close()
 
 
 statistical_words(Directory, Diary_Files_List)
