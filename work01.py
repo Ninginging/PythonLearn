df66a872a51684cd6fd8b1786cdf17907ce2d9cc
Ninginging <86232694+Ninginging@users.noreply.github.com>
2021-07-01 17:28:19 +0800

    homework01修改函数简化

--- work01.py
+++ work01.py
@@ -6,18 +6,22 @@ CODE_LENGTH = 20
 CODE_NUM = 200
 
 
-def get_activation_code():
-    code = ''
-    for i in range(CODE_LENGTH):
-        num = random.randint(0, INIT_STRING_LENGTH)
-        code += INIT_STRING[num]    # 字符串拼接实现激活码生成
-        if (i + 1) % 5 == 0 and i + 1 != CODE_LENGTH:
-            code += '-'
-    return code
+def get_activation_code(length, num):
+    code_list = []
+
+    def get_code():
+        code = ''
+        for __i in range(length):
+            __num = random.randint(0, INIT_STRING_LENGTH)
+            code += INIT_STRING[num]  # 字符串拼接实现激活码生成
+            if (__i + 1) % 5 == 0 and __i + 1 != CODE_LENGTH:
+                code += '-'
+        return code
+
+    for i in range(num):
+        code_list.append(get_code())
+    return code_list
 
 
-CodeList = []
-for i in range(CODE_NUM):
-    CodeList.append(get_activation_code())
-
-print(CodeList)
+Code_List = get_activation_code(CODE_LENGTH, CODE_NUM)
+print(Code_List)
