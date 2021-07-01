6c5780d5105aa91b09de3f401047d2c63d38fa47
Ninginging <86232694+Ninginging@users.noreply.github.com>
2021-07-01 20:26:18 +0800

    homework06完成

--- work04.py
+++ work04.py
@@ -20,23 +20,22 @@ def get_words_list(word_str):
     return word_list
 
 
-def get_statistical_dict(word_list):  # 传入排好顺序的列表,传出键为列表元素,值为元素出现个数的字典
-    temp_dict = {}
-    list_index = 0
-    while list_index < len(word_list)-1:
-        if word_list[list_index] != word_list[list_index + 1]:
-            temp_dict[word_list[list_index]] = word_list.count(word_list[list_index])
-        list_index += 1
-    return temp_dict
+# def get_statistical_dict(word_list):  # 传入排好顺序的列表,传出键为列表元素,值为元素出现个数的字典
+#     temp_dict = {}
+#     list_index = 0
+#     while list_index < len(word_list)-1:
+#         if word_list[list_index] != word_list[list_index + 1]:
+#             temp_dict[word_list[list_index]] = word_list.count(word_list[list_index])
+#         list_index += 1
+#     return temp_dict
 
 
 Word_list = get_words_list(Temp_String)
 Word_Num = len(Word_list)
 # Word_list.sort()  # 列表排序
-print(Word_list)
-print(Word_Num)
-# Word_dict = get_statistical_dict(Word_list)
-# print(Word_dict)
+# print(Word_list)
+# print(Word_Num)
+
 
 Statistical_File.close()
 
--- work06.py
+++ work06.py
@@ -1,7 +1,19 @@
 import os
 import work04
+from collections import Counter
 
-os.chdir('E:\\pythonProject\\PythonLearn\\')
-currentdir = os.getcwd()
-allfile_name = os.listdir(currentdir)
-print(allfile_name)
+Directory = 'source\\DiaryFiles\\'
+Diary_Files_List = os.listdir(Directory)
+
+
+def statistical_words(__dir, files_list):
+    for i in range(len(files_list)):
+        temp_file = open(__dir+files_list[i])
+        temp_string = temp_file.read()
+        word_list = work04.get_words_list(temp_string)
+        words_count_dict = Counter(word_list)
+        print('%s当中出现次数前三多的单词和出现数量是' % Diary_Files_List[i], words_count_dict.most_common(3))
+        temp_file.close
+
+
+statistical_words(Directory, Diary_Files_List)
