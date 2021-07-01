21edd4e108c5184a13edba23fac6691eef7bd912
Ninginging <86232694+Ninginging@users.noreply.github.com>
2021-07-01 20:44:40 +0800

    work06

--- work06.py
+++ work06.py
@@ -12,7 +12,8 @@ def statistical_words(__dir, files_list):
         temp_string = temp_file.read()
         word_list = work04.get_words_list(temp_string)
         words_count_dict = Counter(word_list)
-        print('%s当中出现次数前三多的单词和出现数量是' % Diary_Files_List[i], words_count_dict.most_common(3))
+        statis_results = words_count_dict.most_common(3)
+        print('%s当中出现次数最多的单词是:%s,出现数量是:%s' % (Diary_Files_List[i], statis_results[0][0], statis_results[0][1]))
         temp_file.close()
 
 
