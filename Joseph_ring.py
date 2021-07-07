6c4afd2f24c82227129cd529ce231ae25c5fed6c
Ninginging <86232694+Ninginging@users.noreply.github.com>
2021-07-07 14:21:59 +0800

    约瑟夫环实现

--- Joseph_ring.py
+++ Joseph_ring.py
@@ -1,6 +1,6 @@
-PEO_NUM = 12
-KILL_NUM = 5
-Peo_list = list(range(1, PEO_NUM))
+PEO_NUM = 11
+KILL_NUM = 3
+Peo_list = list(range(1, PEO_NUM + 1))
 kill_list = []  # 初始化玩家编号列表和击杀顺序列表
 
 
@@ -28,3 +28,16 @@ while len(Peo_list) != 1:
     kill_list.append(killed_num)
 
 print('生成击杀的顺序列表为:%s\n最后存活的玩家编号是：%s' % (kill_list, Peo_list[0]))
+
+
+# recursive way
+# 当有n-1个人的时候，编号为i的人对应着上一轮编号为(m+i)%n的人
+# 假设此轮编号为x(n-1)的人最终会留下来,因为编号为x(n-1)的人肯定对应着上一轮的x(n),所以有递推式 x(n)=(m+x(n-1))%n
+def get_kill_rec(n, m):  # 总共有n个人,编号为m的人出列,
+    if n == 1:
+        return 0
+    else:
+        return (m + get_kill_rec(n - 1, m)) % n
+
+
+print('最后存活的玩家编号是：%s' % get_kill_rec(PEO_NUM, KILL_NUM)+1)
